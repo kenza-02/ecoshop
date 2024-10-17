dcc73a0be1c520f7f6017ae8540a97e003c96445
kenza-02 <ahmedmballo7@gmail.com>
2024-10-17 14:13:09 +0000

    Ajout des components de chargements et du module de notification

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { NotificationModule } from './notification';
+import { LoardingComponent } from './loarding/loarding.component';
+import { NoDataComponent } from './no-data/no-data.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoardingComponent, NoDataComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
     RouterModule,
     NgxPaginationModule,
+    NotificationModule,
   ],
   exports: [
     CommonModule,
